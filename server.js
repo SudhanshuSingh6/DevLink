f3f0fed66ab29c3cc015c5c69c07eded047c2d2f
Sudhanshu Singh <sudhanshus026@gmail.com>
2024-02-04 04:26:18 +0530

    mongo db setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.get("/api/posts", require("./routes/api/posts"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
