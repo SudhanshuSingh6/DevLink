91f225dc8da63040981acdab1f689b3d24e313a3
Sudhanshu Singh <sudhanshus026@gmail.com>
2024-02-09 05:22:03 +0530

    added User Api routes and JWT authentication

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const connectDB = require("./config/db");
 
 connectDB();
 
+//Init Middleware
+app.use(express.json);
+
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
