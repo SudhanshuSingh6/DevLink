cc2fb367a47aa9564f5d87cd05e3e9676d44a2c3
SudhanshuSingh6 <sudhanshus026@gmail.com>
2024-02-11 07:13:39 +0530

    added navbar,landing page,register and login forms

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Landing />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  </Router>
+);
+
+export default App;
