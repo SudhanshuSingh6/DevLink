820ef4d52847d68bb2f449129307ff6f87596fca
SudhanshuSingh6 <sudhanshus026@gmail.com>
2024-02-14 14:10:44 +0530

    updated server for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 connectDB();
 
@@ -12,5 +13,11 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname)));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
